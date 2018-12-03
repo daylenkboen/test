9b7d908bd66e76e51a3b6054062ce98093b7a0a6
Daylen <daylenkboen@gmail.com>
2018-12-02 23:10:01 -0800

    V2

--- js/website.js
+++ js/website.js
@@ -45,3 +45,64 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.containerdata .animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('jackInTheBox');
+        $(this).addClass('opacityfull');
+      }
+    });
+  });
+});
+
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.resumeanim .animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeInRight');
+        $(this).addClass('opacityfull');
+      }
+    });
+  });
+});
+
+var bar = new ProgressBar.Circle(progress, {
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  duration: 1400,
+  easing: 'bounce',
+  strokeWidth: 6,
+  from: {color: '#FFEA82', a:0},
+  to: {color: '#ED6A5A', a:1},
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(1.0);  // Number from 0.0 to 1.0
